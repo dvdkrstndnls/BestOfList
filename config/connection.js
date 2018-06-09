0db9b98150b26256555d7ced07c24109ba3edfcb
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-06-09 09:52:49 -0500

    fixed syntax error

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 //for Heroku implementation
-if(process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bestoflist_db"
+  });
 }
-else
-{connection= mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bestoflist_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
